e1300ba72978922cdfc2d51634eb67f7f5a0a8ea
Sujeeth Jinesh <sujeethjinesh@gmail.com>
2016-05-17 02:14:36 -0700

    did problem 10

---
+++ Java Practice/src/Problem10.java
@@ -0,0 +1,29 @@
+/**
+ * Created by lab on 5/17/2016.
+ */
+public class Problem10 {
+
+    public static void main(String[] args) {
+        //uses Sieve of Eratosthenes.
+        boolean[] sieve = new boolean[2000001]; //all false values where first val is 2
+
+        long sum = 0;
+        int counter = 2;
+        int temp = 0;
+
+        for(int i = 2; i < sieve.length; i++) { //sieve of eratosthenes 
+            if (!sieve[i]) {
+                sum += i;
+                for (int j = i; j < sieve.length;) {
+                    temp = i;
+                    sieve[j] = true;
+                    j = counter*temp;
+                    counter++;
+                }
+                counter = 2;
+            }
+        }
+        System.out.println(sum);
+    }
+
+}
---
+++ Java Practice/src/Problem_9.java
@@ -0,0 +1,7 @@
+/**
+ * Created by lab on 5/14/2016.
+ */
+public class Problem_9 {
+
+
+}
